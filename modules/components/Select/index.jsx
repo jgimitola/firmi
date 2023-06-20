b0153db62f99f8f4c3d81d5e42fc13281aa7a2fb
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2023-06-20 15:06:05 -0500

    update component import style

--- modules/components/Select/index.jsx
+++ modules/components/Select/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Autocomplete, TextField } from '@mui/material';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const Select = ({ label, ...props }) => {
   return (
