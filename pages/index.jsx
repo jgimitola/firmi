33e73a8d21275a294db6169aa84b1ed197ff5c8c
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2023-06-20 14:46:00 -0500

    add redirects

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,4 @@ const Home = () => {
   return <></>;
 };
 
-export async function getServerSideProps(context) {
-  return {
-    redirect: {
-      destination: '/login',
-      permanent: false,
-    },
-  };
-}
-
 export default Home;
